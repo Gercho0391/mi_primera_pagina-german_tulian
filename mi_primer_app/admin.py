32df795a405aae2749e76eaee78b45e66facbdb1
German Tulian <germantulian@hotmail.com>
2025-07-09 19:49:46 -0300

    otro commit

--- mi_primer_app/admin.py
+++ mi_primer_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Producto, Cliente, Categoria
 # Register your models here.
+
+modelos_admin = [Producto, Cliente, Categoria]
+
+admin.site.register(modelos_admin)
