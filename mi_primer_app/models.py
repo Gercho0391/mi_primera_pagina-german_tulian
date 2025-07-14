adee1f63b297eefd3e6ee75f1f5e899836169383
German Tulian <germantulian@hotmail.com>
2025-07-14 18:57:07 -0300

    commit

--- mi_primer_app/models.py
+++ mi_primer_app/models.py
@@ -24,3 +24,11 @@ class Cliente(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Mueble(models.Model):
+    nombre = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+    tamano = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
